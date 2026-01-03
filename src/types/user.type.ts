406a550267de99ab4776fd9de05a3d7a0fc01871
SambandhaRai <ginny_30@icloud.com>
2026-01-03 15:35:41 +0545

    User Schema and Type defined

---
+++ src/types/user.type.ts
@@ -0,0 +1,15 @@
+import z from "zod";
+
+export const UserSchema = z.object ({
+    fullName: z.string().trim().min(2, "Full name should be more than 2 characters"),
+    phoneNumber: z.string().trim().min(10, "Phone number can't be less than 10").max(10, "Phone number can't be more than 10"),
+    dob: z.string(),
+    gender: z.string().trim(),
+    bloodGroup: z.string().trim(),
+    healthCondition: z.string().optional(),
+    email: z.email(),
+    password: z.string().trim().min(6, "Password can't be less than 6 characters"),
+    role: z.enum(['admin','user']).default('user'),
+});
+
+export type UserType = z.infer<typeof UserSchema>;
