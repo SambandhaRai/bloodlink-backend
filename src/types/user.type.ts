e5717673864cf054b1699dfe1d49a2f02a24dfda
SambandhaRai <ginny_30@icloud.com>
2026-01-17 17:17:22 +0545

    Updated User Type and Model

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -5,7 +5,7 @@ export const UserSchema = z.object ({
     phoneNumber: z.string().trim().min(10, "Phone number can't be less than 10").max(10, "Phone number can't be more than 10"),
     dob: z.date(),
     gender: z.string().trim(),
-    bloodGroup: z.string().trim(),
+    bloodId: z.string().trim(),
     healthCondition: z.string().optional(),
     email: z.email(),
     password: z.string().trim().min(6, "Password can't be less than 6 characters"),
