9a6bb30948fe93622c415b4f1f7d1c83ced84f40
SambandhaRai <ginny_30@icloud.com>
2026-02-02 19:31:58 +0545

    Added Admin Function

--- src/controllers/admin/admin.controller.ts
+++ src/controllers/admin/admin.controller.ts
@@ -1,6 +1,7 @@
 import { CreateBloodGroupDto } from "../../dtos/blood.dto";
+import { UpdateUserDto } from "../../dtos/user.dto";
 import { AdminService } from "../../services/admin/admin.service";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import z from "zod";
 
 let adminService = new AdminService();
@@ -21,6 +22,75 @@ export class AdminUserController {
         } catch(err: Error | any) {
             return res.status(err.statusCode || 500).json(
                 { success: false, message: err.message || "Internal Server Error" }
+            );
+        }
+    }
+
+    async updateUser(req: Request, res: Response, next: NextFunction) {
+        try{
+            const userId = req.params.id;
+            const parsedData = UpdateUserDto.safeParse(req.body);
+            if(!parsedData.success) {
+                return res.status(400).json({
+                    success: false, errors: z.prettifyError(parsedData.error)
+                });
+            }
+            if(req.file) {
+                parsedData.data.profilePicture = `/uploads/${req.file.filename}`;
+            }
+            const updatedUser = await adminService.updateUser(userId, parsedData.data);
+            return res.status(200).json(
+                { success: true, data: updatedUser, message: "User updated successfully"}
+            );
+        } catch (err: Error | any) {
+            return res.status(err.statusCode || 500).json(
+                { success: false, message: err.message || "Internal server error"}
+            );
+        }
+    }
+
+    async deleteUser(req: Request, res: Response) {
+        try{
+            const userId = req.params.id;
+            const deletedUser = await adminService.deleteUser(userId);
+            if (!deletedUser) {
+                return res.status(404).json(
+                    { success: false, message: "User not found" }
+                );
+            }
+            return res.status(200).json(
+                { success: true, message: "User deleted successfully"}
+            );
+        } catch (err: Error | any) {
+            return res.status(err.statusCode || 500).json(
+                { success: false, message: err.message || "Internal server error"}
+            );
+        }
+    }
+
+    async getAllUsers(req: Request, res: Response) {
+        try {
+            const users = await adminService.getAllUsers();
+            return res.status(200).json(
+                { success: true, data: users, message: "Fetched all users successfully"}
+            );
+        } catch (err: Error | any) {
+            return res.status(err.statusCode || 500).json(
+                { success: false, message: err.message || "Internal server error"}
+            );
+        }
+    }
+
+    async getUserById(req: Request, res: Response) {
+        try {
+            const userId = req.params.id;
+            const user = await adminService.getUserById(userId);
+            return res.status(200).json(
+                { success: true, data: user, message: "Fetched single user successfully" }
+            );
+        } catch (error: Error | any) {
+            return res.status(error.statusCode ?? 500).json(
+                { success: false, message: error.message || "Internal Server Error" }
             );
         }
     }
--- src/services/admin/admin.service.ts
+++ src/services/admin/admin.service.ts
@@ -1,8 +1,11 @@
 import { CreateBloodGroupDto } from "../../dtos/blood.dto";
 import { BloodGroupRepository } from "../../repositories/blood.repository";
+import { UserRepository } from "../../repositories/user.repository";
 import { HttpError } from "../../errors/http-error";
+import { UpdateUserDto } from "../../dtos/user.dto";
 
 let bloodGroupRepository = new BloodGroupRepository();
+let userRepository = new UserRepository();
 
 export class AdminService {
     async createBloodGroup(data: CreateBloodGroupDto){
@@ -12,5 +15,36 @@ export class AdminService {
         }
         const newBloodGroup = await bloodGroupRepository.createBloodGroup(data);
         return newBloodGroup;
+    }
+
+    async getAllUsers() {
+        const users = await userRepository.getAllUsers();
+        return users;
+    }
+
+    async getUserById(id: string) {
+        const user = await userRepository.getUserById(id);
+        if(!user) {
+            throw new HttpError(404, "User not found");
+        }
+        return user;
+    }
+
+    async updateUser(id: string, data: UpdateUserDto) {
+        const user = await userRepository.getUserById(id);
+        if(!user) {
+            throw new HttpError(404, "User not found");
+        }
+        const updatedUser = await userRepository.updateOneUser(id, data);
+        return updatedUser;
+    }
+
+    async deleteUser(id: string) {
+        const user = await userRepository.getUserById(id);
+        if(!user) {
+            throw new HttpError(404, "User not found");
+        }
+        const deletedUser = await userRepository.deleteOneUser(id);
+        return deletedUser
     }
 }
