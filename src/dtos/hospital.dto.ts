f7e236f6793b34dfb583a55ac6b0829f2b3d037b
SambandhaRai <ginny_30@icloud.com>
2026-02-12 18:30:39 +0545

    Created Hospital Type, Model and DTO

---
+++ src/dtos/hospital.dto.ts
@@ -0,0 +1,17 @@
+import z from "zod";
+import { HospitalSchema } from "../types/hospital.type";
+
+export const CreateHospitalDto = HospitalSchema.pick({
+  name: true,
+  location: true,
+});
+export type CreateHospitalDto = z.infer<typeof CreateHospitalDto>;
+
+export const UpdateHospitalDto = HospitalSchema.pick({
+  name: true,
+  location: true,
+}).extend({
+    isActive: z.boolean().optional(),
+}).partial();
+
+export type UpdateHospitalDto = z.infer<typeof UpdateHospitalDto>;
